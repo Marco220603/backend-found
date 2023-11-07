90cea2a790836ac051f892d1ce3a3f288effd61e
Marco220603 <133729769+Marco220603@users.noreply.github.com>
2023-11-07 18:54:33 -0500

    fff

--- src/app/model/song.ts
+++ src/app/model/song.ts
@@ -1,6 +1,6 @@
 export class Songs{
     idSong:number=0;
-    nameSong:string="hola";
+    nameSong:string="";
     idGender:number=0;
     idArtist:number=0;
     fechaSong:Date = new Date(Date.now())
