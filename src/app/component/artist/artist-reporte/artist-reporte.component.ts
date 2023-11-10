99d666ef3482cdf0438b60ce233f33fc5e2f3fa6
Cesar Arenas <u201920972@upc.edu.pe>
2023-11-10 12:06:10 -0500

    Artist

--- src/app/component/artist/artist-reporte/artist-reporte.component.ts
+++ src/app/component/artist/artist-reporte/artist-reporte.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+import { Gender } from 'src/app/model/gender';
+import { ArtistService } from 'src/app/services/artist.service';
 
 @Component({
   selector: 'app-artist-reporte',
   templateUrl: './artist-reporte.component.html',
   styleUrls: ['./artist-reporte.component.css']
 })
-export class ArtistReporteComponent {
+export class ArtistReporteComponent implements OnInit {
+  dataSource:MatTableDataSource<Gender> = new MatTableDataSource<Gender>();
+  fechaForm : FormGroup = new FormGroup({});
+  mensaje: string= '';
+  fechaVacia:boolean = false;
 
+  displayedColums: string[] = ['codigo','nombre','genero'];
+
+  constructor(private aS:ArtistService,private formBuilder:FormBuilder){  }
+
+  ngOnInit(): void {
+      this.fechaForm = this.formBuilder.group({
+        fecha: [null,Validators.required],
+      });
+  }
+
+  buscar(){
+    if(this.fechaForm.valid){
+      const fechas = this.fechaForm.value.fecha.toISOString().substring(0,10);
+      this.aS.buscar(fechas).subscribe((data)=>{
+        this.dataSource.data=data;
+        if(data.length===0){
+          this.mensaje=" No se encontraron resultados para la fecha seleccionada.";
+        }else{
+          this.mensaje='';
+        }
+      });
+    }else{
+      if(this.fechaForm.get('fecha')?.hasError('required')){
+         this.mensaje='ingrese fecha' 
+      }
+    }
+  }
+
+  obtenerControlCampo(nombreCampo:string):AbstractControl{
+    const control = this.fechaForm.get(nombreCampo);
+    if(!control){
+      throw new Error(`Control no encontrado para el campo ${nombreCampo}`);
+    }
+    return control;
+  }
 }
+
--- src/app/component/artist/listar-artist/listar-artist.component.ts
+++ src/app/component/artist/listar-artist/listar-artist.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Artist } from 'src/app/model/artist';
+import { ArtistService } from 'src/app/services/artist.service';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-listar-artist',
   templateUrl: './listar-artist.component.html',
   styleUrls: ['./listar-artist.component.css']
 })
-export class ListarArtistComponent {
 
+export class ListarArtistComponent implements OnInit {
+  dataSource: MatTableDataSource<Artist>= new MatTableDataSource();
+  displayedColumns: string[] = ['codigo','nombre', 'genero','accion01','accion02'];
+  @ViewChild(MatPaginator) paginator!:MatPaginator;
+  constructor(private aS:ArtistService,private loginService:LoginService) {}
+ngOnInit(): void {
+  this.aS.list().subscribe(data=>{
+    this.dataSource=new MatTableDataSource(data);
+    this.dataSource.paginator = this.paginator;
+  })
+  this.aS.getList().subscribe((data)=>{
+    this.dataSource = new MatTableDataSource(data);
+    this.dataSource.paginator = this.paginator;
+  });
+ }
+ eliminar(id:number){
+  this.aS.delete(id).subscribe((data)=>{
+    this.aS.list().subscribe((data)=>{
+      this.aS.setList(data);
+    })
+  })
+ }
+ filter(en:any){
+  this.dataSource.filter = en.target.value.trim();
+ }
+ role:String=""
+ verificar() {
+  this.role=this.loginService.showRole();
+  return this.loginService.verificar();
 }
+}
+
+
+
+
+
