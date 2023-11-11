7513ab7fee3287c3327fd8129b17cd19a084ff19
Cristian Martin Arias Saldaña <142331430+Lacanchaa@users.noreply.github.com>
2023-11-10 19:42:42 -0500

    song.component.ts

--- src/app/component/song/song.component.ts
+++ src/app/component/song/song.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-song',
   templateUrl: './song.component.html',
   styleUrls: ['./song.component.css']
 })
-export class SongComponent {
-
+export class SongComponent{
+  constructor(public route: ActivatedRoute) {}
+  ngOnInit(): void{}
 }
+
